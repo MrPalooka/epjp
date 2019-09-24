9e64acdd3a4358b31079545126b811d84cf5a1e4
Administrator <Administrator@192.168.20.62>
2019-09-24 15:10:43 +0200

    dw2d2 1.0.2

--- src/s081/Time8.java
+++ src/s081/Time8.java
@@ -1,11 +1,13 @@
 package s081;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 
 public class Time8 {
     public static void main(String[] args) {
@@ -20,5 +22,10 @@ public class Time8 {
 
         LocalDateTime ldt = LocalDateTime.of(date, time);
         System.out.println(ldt);
+        
+        Duration x = Duration.of(12, ChronoUnit.DAYS);
+        System.out.println(x);
+        Duration d = Duration.ofDays(2);
+        System.out.println(d);
     }
 }
